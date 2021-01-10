be5750f655559c24ce260890376d398091b86567
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-01-10 22:47:05 +0700

    add lcd, api post, and modify program flow

--- gate.ino
+++ gate.ino
@@ -1,114 +1,115 @@
+#include <LiquidCrystal_I2C.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 #include <SPI.h>
 #include <MFRC522.h>
-#include <ESP8266WiFi.h> 
-#include <ESP8266HTTPClient.h>
-#include <WiFiClient.h>
 
-#define RST_PIN D1
-#define SDA_PIN D2
+#define RST_PIN D3
+#define SDA_PIN D4
 
-const char* ssid     = "Tolong Jangan Dipakai";
-const char* password = "janganmaling";
+String wifi_ssid = "Tolong Jangan Dipakai";
+String wifi_pass = "janganmaling";
 
-String serverName = "http://192.168.100.36:8080/api";
+String server_address = "http://192.168.1.1:8080";
+String gate_id = "5ff98afe90384e440c3d6bec";
+
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
 
 MFRC522 mfrc522(SDA_PIN, RST_PIN);
 
+void lcdPrint(String a, String b) {
+  lcd.clear();
+
+  lcd.setCursor(0, 0);
+  lcd.print(a);
+
+  lcd.setCursor(0, 1);
+  lcd.print(b);
+
+  Serial.println(a + " " + b);
+}
+
 void setup() {
-  pinMode(D4, OUTPUT);
-  digitalWrite(D4, HIGH);
-  
   Serial.begin(9600);
 
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-  }
-
-  Serial.println("Connection established!");  
-  Serial.print("IP address:\t");
-  Serial.println(WiFi.localIP());
+  lcd.begin(16,2);
+  lcd.init();
+  lcd.backlight();
 
   SPI.begin();
   mfrc522.PCD_Init();
-  Serial.println("Put your card to the reader...");
-  
-  digitalWrite(D4, LOW);
 }
 
 void loop() {
-  if(WiFi.status() !=  WL_CONNECTED){
-    return;
-  }
-  
-  if(!mfrc522.PICC_IsNewCardPresent()){
-    return;
-  }
+  if (WiFi.status() !=  WL_CONNECTED) {
+    lcdPrint("Menghubungkan ke", wifi_ssid);
 
-  if(!mfrc522.PICC_ReadCardSerial()){
-    return;
-  }
-
-  Serial.print("UID tag :");
-  String content = "";
-  byte letter;
-
-  for(byte i = 0; i < mfrc522.uid.size; i++){
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
-    Serial.print(mfrc522.uid.uidByte[i], HEX);
-    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : ""));
-    content.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
-
-  content.toUpperCase();
-
-  Serial.println("");
-  Serial.println("UID tag :" + content);
-      
-  HTTPClient http;
-  
-  String serverPath = serverName + "/card?tagId=" + content;
-  http.begin(serverPath);
-
-  Serial.println(serverPath);
-  int httpResponseCode = http.GET();
-
-  if (httpResponseCode > 0) {
-    if (http.getString() != "[]") {
-      Serial.println("Kartu cocok");
-      digitalWrite(D4, HIGH);
-    } else {
-      Serial.println("Kartu tidak cocok");
-      digitalWrite(D4, LOW);
+    WiFi.begin(wifi_ssid, wifi_pass);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
     }
+
+    lcdPrint("Terhubung ke", "WiFi");
+    delay(2000);
+
+    lcdPrint("Alamat IP", WiFi.localIP().toString());
+    delay(2000);
+
+    lcdPrint("Mengubungkan ke", server_address);
+
+    HTTPClient http;
+    http.begin(server_address);
+
+    int http_rc = http.GET();
+    while (http_rc <= 0) {
+      http_rc = http.GET();
+      delay(1000);
+    }
+
+    http.end();
+
+    lcdPrint("Terhubung ke", "server");
+    delay(2000);
+  }
+
+  lcdPrint("Silahkan tempel", "kartu");
+  if (!mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  if (!mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  String tag_id = "";
+  for(byte i = 0; i < mfrc522.uid.size; i++){
+    tag_id.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? "0" : ""));
+    tag_id.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  tag_id.toUpperCase();
+
+  lcdPrint("Kartu terdeteksi:", tag_id);
+  delay(2000);
+
+  lcdPrint("Memverifikasi", "kartu");
+
+  HTTPClient http;
+  String api_address = server_address + "/api/device/visitation/";
+  http.begin(api_address + gate_id + "/" + tag_id);
+
+  int http_rc = http.GET();
+  if (http_rc >= 0) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+
+    lcd.print(http.getString());
+    Serial.println(http.getString());
+    delay(2000);
   }
   else {
-    Serial.print("Error code: ");
-    Serial.println(httpResponseCode);
+    lcdPrint("Kesalahan alat", "");
+    delay(2000);
   }
 
   http.end();
-  
-  delay(1000);
-  
-//  if(content.substring(1) == "D0 F2 8D 32"){
-//    digitalWrite(D4, HIGH);se
-//    Serial.println("Kartu cocok");
-//    Serial.println();
-//    delay(1000);
-//  }
-//
-//  else if(content.substring(1) == "79 D0 24 A3"){
-//    digitalWrite(D4, HIGH);
-//    Serial.println("Kartu cocok");
-//    Serial.println();
-//    delay(1000);
-//  }
-//
-//  else{
-//    digitalWrite(D4, LOW);
-//    Serial.println("Kartu Tidk cocok");
-//    delay(1000);
-//  }
-
 }
